e0629f3aa5ff022ca434883f6d152a470f1b52cd
shu yusa <shu.yusa@gmail.com>
2016-11-20 01:28:35 +0900

    チュートリアルの残りのコードを追加

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+  moduleId: module.id,
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <!-- この要素の下にルーティング毎のviewが表示される -->
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app.component.css'],
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
